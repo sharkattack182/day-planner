f30b048cef8cc2c6a4072dce462b66d2d80145df
Matthew Courtney <matthewdc182@gmail.com>
2020-12-08 22:51:55 -0500

    added in some psudo code need to revist moment.js before I really get started

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 var hourlyDisplay = $(".container");
 var currentDay = $("#currentday");
+// nned to build the html dthrough jquery but need to do it by the predefined classes
+// create a list fo classes and what the do
+// check the example and map it out on paper
+// the save  button will have an click function to log the text content to local storage
+// on load will render items from local storage each
+// will need each div to have a value according to hour
+//      military time
+// need to set current hour and track the hour to set colors in the divs
+
